a088722df7f49b8e19c1d3f5e58720c266a757ef
DTAGSONOMY\miguel.rameau <miguel.rameau@touchvie.com>
2017-09-26 10:38:06 +0200

    Checking first selected when change scene.

--- src/components/miniCardList/miniCardList.component.tsx
+++ src/components/miniCardList/miniCardList.component.tsx
@@ -47,12 +47,12 @@ export class MiniCardListClass extends React.Component<MiniCardListProps, {}> {
                 this.props.setNodeById(this.props.idx);
             }
 
-            if(this.props.elements.length > 0 && nextProps.elements.length === 0){
+            if (this.props.elements.length > 0 && nextProps.elements.length === 0) {
                 if (ReactDOM.findDOMNode(this).querySelector(".childFocused")) {
                     this.props.setSelectedOnSceneChange(true);
                 }
             }
-            
+
 
             if (this.props.setSelectedOnSceneChange !== undefined && this.props.elements.length === 0 && nextProps.elements.length > 0) {
                 this.props.setSelectedOnSceneChange(false);
@@ -65,7 +65,7 @@ export class MiniCardListClass extends React.Component<MiniCardListProps, {}> {
         const limit = 3;
         if (card.relations instanceof Array) {
             // card.relations.map((el: Single | Duple, index: number) => {
-            for(const el of card.relations){
+            for (const el of card.relations) {
                 const rel = el as Single | Duple;
                 // console.log("[MiniCardList][getRelations]: ", rel);
                 switch (rel.content_type) {
