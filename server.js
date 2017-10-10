df7dd7fd793fe7dc1108c8682df42c068514ac2a
Daniel Mariño Ruiz <daniel.marino@touchvie.com>
2017-10-10 14:38:33 +0200

    show ip on server

--- server.js
+++ server.js
@@ -6,6 +6,33 @@ var proxy = require('express-http-proxy');
 const isProduction = process.argv.indexOf('-p') >= 0 || process.env.NODE_ENV == "production";
 console.log("SERVER is PRoduction?", isProduction, process.env.NODE_ENV);
 
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var localIp = '';
+
+Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
+
+    ifaces[ifname].forEach(function (iface) {
+        if ('IPv4' !== iface.family || iface.internal !== false) {
+            // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+            return;
+        }
+
+        if (alias >= 1) {
+            // this single interface has multiple ipv4 addresses
+            console.log(ifname + ':' + alias, iface.address);
+        } else {
+            // this interface has only one ipv4 adress
+            console.log(ifname, iface.address);
+        }
+        if (ifname == 'en0') {
+            localIp = iface.address;
+        }
+        ++alias;
+    });
+});
+
 const app = express(),
     DIST_DIR = path.join(__dirname, "dist"),
     PORT = 3000;
@@ -66,6 +93,6 @@ if (!isProduction) {
 }
 
 app.listen(PORT, () => {
-    console.log("Webpack dev server (Custom for HBBTV) listening on port", PORT);
+    console.log(`Webpack dev server (Custom for HBBTV) listening on ${localIp}:${PORT}`);
     console.log("Waiting for webpack build");
 }); 
