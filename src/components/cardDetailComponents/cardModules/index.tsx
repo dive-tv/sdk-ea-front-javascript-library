cdcf99a48938c81313d95f8099724d718740bf75
DTAGSONOMY\miguel.rameau <miguel.rameau@touchvie.com>
2018-01-15 12:41:44 +0100

    Fixing paths for travis.

--- src/components/cardDetailComponents/cardModules/index.tsx
+++ src/components/cardDetailComponents/cardModules/index.tsx
@@ -3,7 +3,7 @@ export * from './cardModule.base.component';
 export * from './header/header.component';
 export * from './text/text.component';
 export * from './table/table.component';
-export * from './list/list.component';
+export * from './List/list.component';
 export * from './awards/awards.component';
 export * from './map/map.component';
 export * from './banner/banner.component';
