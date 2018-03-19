8ad39b922794dd70c6ebc910dad093916769f89b
DTAGSONOMY\miguel.rameau <miguel.rameau@touchvie.com>
2018-03-19 10:57:48 +0100

    beta version

--- scr/publish.sh
+++ scr/publish.sh
@@ -8,7 +8,14 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         npm run build
         npm version patch
         npm publish
-
+        
+    elif [ "$TRAVIS_BRANCH" == "beta" ]; then
+        echo "beta version"
+        echo "Set minor version and push on master branch, publishing lib to NPM"
+        npm install
+        npm run build
+        npm version patch
+        npm publish --tag beta
     else
         echo "Branch is not master, skipping NPM publish"
     fi
